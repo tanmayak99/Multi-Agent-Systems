9e99d2b50e67fb57f31b6d2acdded79a431d378a
tanmayak99 <34204513+tanmayak99@users.noreply.github.com>
2021-06-01 14:51:26 +0530

    Update main.m

--- Travelling Salesman Problem/main.m
+++ Travelling Salesman Problem/main.m
@@ -1,28 +1,3 @@
-%
-% Copyright (c) 2015, Mostapha Kalami Heris & Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "LICENSE" file for license terms.
-%
-% Project Code: YPEA103
-% Project Title: Ant Colony Optimization for Traveling Salesman Problem
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Cite as:
-% Mostapha Kalami Heris, Ant Colony Optimization in MATLAB (URL: https://yarpiz.com/53/ypea103-ant-colony-optimization), Yarpiz, 2015.
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
+%main file for implementing ACO
 
-%aco;
- x = [82 91 12 92 63 9 28 55 96 97 15 98 96 49 80 14 42 92 80 96];
-    
-    y = [66 3 85 94 68 76 75 39 66 17 71 3 27 4 9 83 70 32 95 3];
-    
-    scatter(x,y,100,'MarkerEdgeColor','black',...
-              'MarkerFaceColor','yellow',...
-              'LineWidth',1.5)
-    %'k-o', ...
-     %   'MarkerSize', 10, ...
-      %  'MarkerFaceColor', 'y', ...
-       % 'LineWidth', 1.5);
+aco;
