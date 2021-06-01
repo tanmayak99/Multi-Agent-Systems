8950631e679896e96aad807523680e32eb7b52f4
tanmayak99 <34204513+tanmayak99@users.noreply.github.com>
2021-06-01 14:54:13 +0530

    Update TourLength.m

--- Travelling Salesman Problem/TourLength.m
+++ Travelling Salesman Problem/TourLength.m
@@ -1,18 +1,3 @@
-%
-% Copyright (c) 2015, Mostapha Kalami Heris & Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "LICENSE" file for license terms.
-%
-% Project Code: YPEA103
-% Project Title: Ant Colony Optimization for Traveling Salesman Problem
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Cite as:
-% Mostapha Kalami Heris, Ant Colony Optimization in MATLAB (URL: https://yarpiz.com/53/ypea103-ant-colony-optimization), Yarpiz, 2015.
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
 
 function L = TourLength(tour, model)
 
