f07e80e62d97c9fda6e525a1a58fee92cd60202d
tanmayak99 <34204513+tanmayak99@users.noreply.github.com>
2021-06-01 14:52:26 +0530

    Update CreateModel.m

--- Travelling Salesman Problem/CreateModel.m
+++ Travelling Salesman Problem/CreateModel.m
@@ -1,19 +1,3 @@
-%
-% Copyright (c) 2015, Mostapha Kalami Heris & Yarpiz (www.yarpiz.com)
-% All rights reserved. Please read the "LICENSE" file for license terms.
-%
-% Project Code: YPEA103
-% Project Title: Ant Colony Optimization for Traveling Salesman Problem
-% Publisher: Yarpiz (www.yarpiz.com)
-% 
-% Developer: Mostapha Kalami Heris (Member of Yarpiz Team)
-% 
-% Cite as:
-% Mostapha Kalami Heris, Ant Colony Optimization in MATLAB (URL: https://yarpiz.com/53/ypea103-ant-colony-optimization), Yarpiz, 2015.
-% 
-% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
-%
-
 function model = CreateModel()
 
     x = [82 91 12 92 63 9 28 55 96 97 15 98 96 49 80 14 42 92 80 96];
